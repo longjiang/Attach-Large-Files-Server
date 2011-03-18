dda27cf6e639295754aedc9b02445e47ae53db7c
Jiang Long <longjiang2005@gmail.com>
2011-03-18 11:44:10 -0700

    Added application folder

---
+++ application/controllers/welcome.php
@@ -0,0 +1,18 @@
+<?php
+
+class Welcome extends Controller {
+
+	function Welcome()
+	{
+		parent::Controller();	
+	}
+	
+	function index()
+	{
+		echo 'Welcome Controller loaded';
+		//Nothing displays
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./system/application/controllers/welcome.php */
---
+++ application/views/download_view.php
@@ -0,0 +1,11 @@
+
+
+<h1>Large Attachment Notice</h1>
+
+<p>The following attachments are large and download will take time.  Even if the download seems to take forever to start, please wait patiently for it will start and come to a completion.</p>
+<p>Click to download:</p>
+<code>
+	<a href="<?php echo $filepath; ?>"><?php echo $filename; ?></a><br/><br/>
+	<?php echo round($filesize / 1024 / 1024, 1); ?> MB;  Estimated download time: <?php echo round($filesize / 102400 / 60, 0); ?> minutes.
+</code>
+<p class="footnote">Tip: To prevent PDF files from opening in the browser window, right click and choose "Save link as..."</p>
